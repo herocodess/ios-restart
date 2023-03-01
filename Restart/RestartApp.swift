d09ee049a959d61b5ba4a0e4ddc85881dbfa6de8
Momoh Hero <42640073+herocodess@users.noreply.github.com>
2023-03-01 19:26:40 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Hero Momoh on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
